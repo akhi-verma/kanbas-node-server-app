de33f9b1ca55bc4e94c4ca04f2feff4754cb45ee
Akhilesh Verma <145293359+akhi-verma@users.noreply.github.com>
2023-12-12 11:24:51 -0500

    Update app.js - try and fix

--- app.js
+++ app.js
@@ -13,20 +13,27 @@ import UserRoutes from "./users/routes.js";
 import session from "express-session";
 
 const app = express();
-app.use(express.json());
 app.use(cors(
-    {credentials: true,
-        origin: "http://localhost:3000",
-    }
+{credentials: true,
+  origin: http://localhost:3000
+}
 ));
+
 const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
-  };
-  app.use(
-    session(sessionOptions)
-  );  
+secret: "any string",
+resave: false,
+saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+sessionOptions.proxy = true;
+sessionOptions.cookie = {
+  sameSite: "none",
+  secure: true,
+};
+}
+app.use(session(sessionOptions));  
+
+app.use(express.json());
 Hello(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
