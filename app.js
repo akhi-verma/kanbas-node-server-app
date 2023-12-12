a7d8481db75475d1db3a09bbefd36e5edf45919b
Akhilesh Verma <145293359+akhi-verma@users.noreply.github.com>
2023-12-12 11:28:06 -0500

    Update app.js - try and fix

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ import UserRoutes from "./users/routes.js";
 import session from "express-session";
 
 const app = express();
-app.use(express.json());
 app.use(cors(
     {credentials: true,
-        origin: process.env.FRONTEND_URL
+      origin: process.env.FRONTEND_URL
     }
 ));
+
 const sessionOptions = {
     secret: "any string",
     resave: false,
@@ -31,7 +31,9 @@ const sessionOptions = {
       secure: true,
     };
   }
-  app.use(session(sessionOptions));  
+app.use(session(sessionOptions));  
+
+app.use(express.json());
 Hello(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
